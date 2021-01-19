84fe88a76dcb041196ac0e8603147d97a5b7f451
lxily <im.lch@foxmail.com>
2021-01-19 13:11:15 +0800

    更新167解决方案

--- 0101-0200/167-两数之和 II - 输入有序数.cpp
+++ 0101-0200/167-两数之和 II - 输入有序数.cpp
@@ -1,10 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 /*
-思路：
+思路：尺取法 O(n)
 */
 //start here
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l=0,r=numbers.size()-1;
+        while(l<r){
+            if(numbers[l]+numbers[r]==target)return {l+1,r+1};
+            if(numbers[l]+numbers[r]>target)r--;
+            else l++;
+        }
+        return {-1,-1};
+    }
+};
 //
 
 int main(){
